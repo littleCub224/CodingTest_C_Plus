23ba6b16a9e925ef1320631475939fe41a59bbef
littleCub224 <ykim2023@unijet.co.kr>
2024-10-18 17:35:42 +0900

    DFS 2 Dimension

--- DFS_2_Dimension/DFS_2_Dimension.cpp
+++ DFS_2_Dimension/DFS_2_Dimension.cpp
@@ -1,28 +1,68 @@
 ﻿#include <iostream>
 #include <vector>
 using namespace std;
-const int n = 6;
-vector<int> adj[n];
-int visited[n];
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0, 1, 0, -1 };
+int rowCount, colCount, k, y, x, ret, ny, nx, t;
+int a[104][104];
+bool visited[104][104];
 
 
-void DFS(int u) {
-	visited[u] = 1;
-	cout << u << "\n";
-	for (int v : adj[u]) {
-		if (visited[v] == 0) {
-			DFS(v);
+void DFS(int y, int x)
+{
+	visited[y][x] = 1;
+	cout << y << " : "<< x << "\n";
+	
+	for (int i = 0; i < 4; i++)
+	{
+		ny = y + dy[i];
+		nx = x + dx[i];
+
+		if (ny < 0 || nx < 0 || ny >= colCount || nx >= rowCount) continue;
+
+		if (a[ny][nx] == 1 && !visited[ny][nx])
+		{
+			DFS(ny, nx);
 		}
 	}
-	cout << u << "로부터 시작된 함수가 종료되었습니다.\n";
 	return;
 }
 
+
 int main() {
-	adj[1].push_back(2);
-	adj[1].push_back(3);
-	adj[2].push_back(4);
-	adj[4].push_back(2);
-	adj[2].push_back(5);
-	DFS(1);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> colCount >> rowCount;
+
+	for (int col = 0; col < colCount; col++)
+	{
+		for (int row = 0; row < rowCount; row++)
+		{
+			cin >> a[col][row];
+		}
+	}
+
+
+
+
+	for (int col = 0; col < colCount; col++)
+	{
+		for (int row = 0; row < rowCount; row++)
+		{
+			if (a[col][row] == 1 && !visited[col][row])
+			{
+				cout << col << " : " << row << " dfs 시작\n";
+	
+				DFS(col, row);
+				ret++;
+			}
+			
+		}
+	}
+
+	cout << ret;
+
+	return 0;
+
 }
